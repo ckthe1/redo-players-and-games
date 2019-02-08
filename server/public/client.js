5838c959eee3b954ca0712e20676a96def4cb040
Ck T <chengkou@gmail.com>
2019-02-08 13:47:13 -0600

    add empty players and opp drop box

--- server/public/client.js
+++ server/public/client.js
@@ -3,19 +3,17 @@ $(document).ready(onReady);
 function onReady(){
     console.log('in jQuery');
     $('#addPlayersButton').on('click', inputPlayers)
-   
+    
     $.ajax({ // ajax go to server
         url: '/players',
         method: 'GET'//reqst to server
-
+        
     }).then(function (response){
-         $('playersNameIn').empty();
-            $('.ulPlayers').empty();
+        
+        $('.ulPlayers').empty();
         response.forEach(function(players) {
             $('.ulPlayers').append(`
-         
             <li class= playersNameOut>${players.name}</li>
-        
           `);
         });
         // for (let i = 0; i < response.length; i++) {//response.foreach(wolfObject)=>
@@ -31,34 +29,46 @@ function inputPlayers(){
     // let newPlayers = $('#playersNameIn').val();
     // console.log('newplyaers:', newPlayers);
     let newPlayers = $('#playersNameIn').val();
+    $('#playersNameIn').val('');//cannot empty(), have to use val('')
     // $('.ulPlayers').empty();
     $.ajax({//use ajax, send them to the server
         url:'/players', 
         method: 'POST',
         data:{
             name: newPlayers, 
-        }
+         }
 
     }).then(function(){
+        getAllPlayers();
+        });
+}
+function getAllPlayers(){
         $.ajax({ // ajax go to server
             url: '/players',
             method: 'GET'//reqst to server
 
         }).then(function (response) {
             $('.ulPlayers').empty();
+            $('#dropDownPlayer').empty();
+            $('#dropDownOpponent').empty();
             for (let i = 0; i < response.length; i++) {
                 console.log(response[i]);
                 let players = response[i];
                 $('.ulPlayers').append(`  
-            <li class= playersNameOut>${players.name}</li>       
-        `);
+                 <li class= playersNameOut>${players.name}</li>       
+                 `);
 
+                
                 $('#dropDownPlayer').append(`
                 <option>${players.name}</option>
                 `);
+                // 
+                
+                $('#dropDownOpponent').append(`
+                <option>${players.name}</option>
+                `)
+                // 
             }
         })// end response function
-    })//end ajax
-
-};// end inputPlayers
-
+    //end ajax
+};
